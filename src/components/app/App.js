f845e51274aff5fd47dbc43925669f78f2783a7b
mahout <93513481+muhahoi@users.noreply.github.com>
2024-11-13 09:16:04 +0300

    Сформирована начальная структура папок, установлены необходимые зависимости

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import './../../style/style.scss';
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1>hello</h1>
+			<a href=''>link</a>
+		</div>
+	);
+};
+
+export default App;
